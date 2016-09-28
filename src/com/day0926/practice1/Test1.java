fe007c7e47efef044ba0b0ff0cfc108533ebc202
txt <tongxiaotong139@163.com>
2016-09-28 20:56:16 +0800

    version_0928

--- src/com/day0926/practice1/Test1.java
+++ src/com/day0926/practice1/Test1.java
@@ -1,13 +1,18 @@
 package com.day0926.practice1;
 
+import static org.junit.Assert.*;
+
+import org.junit.Ignore;
+import org.junit.Test;
+
 public class Test1 {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		int a=123;
-		int b =120;
-		System.out.println(sum(a, b));
-	}
+//	public static void main(String[] args) {
+//		// TODO Auto-generated method stub
+//		int a=123;
+//		int b =120;
+//		System.out.println(sum(a, b));
+//	}
 	public static int sum(int num1, int num2){
 		return reverse(num1)+reverse(num2);
 	}
@@ -19,5 +24,10 @@ public class Test1 {
 			n=n*10+temp;
 		}
 		return n;
+	}
+	
+	@Test
+	public void test1(){
+		assertEquals(33, sum(210, 120));
 	}
 }
--- src/com/day0926/practice2/Test2.java
+++ src/com/day0926/practice2/Test2.java
@@ -1,13 +1,17 @@
 package com.day0926.practice2;
 
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
 public class Test2 {
-	public static void main(String[] args) {
-		long start = System.currentTimeMillis();
-//		System.out.println(Calculate(40));
-		long end = System.currentTimeMillis();
-		long t = end-start;
-		System.out.println((1<<3)|(1<<2));
-	}
+//	public static void main(String[] args) {
+////		long start = System.currentTimeMillis();
+//		System.out.println(Calculate(20));
+////		long end = System.currentTimeMillis();
+////		long t = end-start;
+////		System.out.println((1<<3)|(1<<2));
+//	}
 
 	public static long Calculate(long num) {
 		if (num == 1) {
@@ -19,4 +23,8 @@ public class Test2 {
 		}
 	}
 
+	@Test
+	public void test1(){
+		assertEquals(21,Calculate(3));
+	}
 }
